359713842f0e7a03313d13bfa98e759f432e379e
jorge <llanezarodriguezjorge@gmail.com>
2025-12-29 12:53:14 +0100

    added auxiliary request methods

--- tests/simple_request_test.rs
+++ tests/simple_request_test.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 use std::thread;
 use std::sync::mpsc;
 use ll_simple_httpserver;
-use ll_simple_httpserver::http::parser::Verb;
+use ll_simple_httpserver::http::parser::{HttpRequest, Verb};
 use ll_simple_httpserver::http::uri::Uri;
 
 #[test]
@@ -55,4 +55,33 @@ fn server(tx: mpsc::Sender<SocketAddr>) {
         }
 
     }
+}
+fn str_to_http_bytes(input: &str) -> Vec<u8> {
+    input
+        .replace("\r\n", "\n") // Step 1: Normalize all line endings to LF (\n)
+        .replace("\n", "\r\n") // Step 2: Convert all LF to CRLF (\r\n)
+        .into_bytes()          // Step 3: Consume the String and return the bytes
+}
+#[test]
+fn all_headers_parsing_test() {
+    let raw_request = r#"POST /api/v1/update HTTP/1.1
+Host: localhost:8080
+Accept: application/json
+Authorization: Basic dXNlcjpwYXNz
+Referer: https://google.com/
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+Content-Length: 27
+Content-Type: application/json
+X-Custom-Debug-ID: 99b1-az22
+
+{"status": "testing-rust"}"#;
+
+    // Convert to the wire-ready Vec<u8>
+    let request_buffer = str_to_http_bytes(raw_request);
+
+    let request_str = str::from_utf8(&request_buffer).unwrap();
+
+    let parsed_request = HttpRequest::from_str(request_str).unwrap();
+    assert_eq!(parsed_request.get_headers().len(), 8);
+    assert_eq!(parsed_request.get_meth);
 }
